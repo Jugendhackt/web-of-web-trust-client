01dd17776ab0eae3e7370323f30de07df4699243
Leo Maroni <git@em0lar.de>
2021-10-10 10:32:15 +0200

    Add serverURL option

--- src/options/storage.js
+++ src/options/storage.js
@@ -2,6 +2,7 @@ import OptionsSync from 'webext-options-sync';
 
 export default new OptionsSync({
     defaults: {
+        serverUrl: "http://127.0.0.1:8000/",
         weightA: 1,
         weightB: 1
     },
